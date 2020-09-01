8250fbdaf9b2e47f718cabe6e348879196978635
argierdfj <argierincon@gmail.com>
2020-09-01 11:56:47 -0500

    añadiendo configuración de firebase al proyecto

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import "./styles.scss";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './styles.scss';
+import firebase from 'firebase';
 
+var firebaseConfig = {
+  apiKey: 'AIzaSyDFWK644f3VPzx-TdThbWJll302ww_0Cts',
+  authDomain: 'latintv-d2744.firebaseapp.com',
+  databaseURL: 'https://latintv-d2744.firebaseio.com',
+  projectId: 'latintv-d2744',
+  storageBucket: 'latintv-d2744.appspot.com',
+  messagingSenderId: '719937038099',
+  appId: '1:719937038099:web:7aff2b5fc6cc2c7f2c9ce6',
+  measurementId: 'G-R0T2EN0E06',
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-var mountNode = document.getElementById("app");
+var mountNode = document.getElementById('app');
 ReactDOM.render(<App name="Jane" />, mountNode);
