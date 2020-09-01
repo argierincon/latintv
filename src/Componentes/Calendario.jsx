1a96c57bcf27afaba0dd596c2527a4cb35177357
verenisseeyzaguirre <verenisse.eyzaguirre@gmail.com>
2020-09-01 17:23:48 -0500

    vista tabla de programacion

--- src/Componentes/Calendario.jsx
+++ src/Componentes/Calendario.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import React from 'react';
+import '../assets/estilos/contenedores/calendario.scss';
 
 export const Calendario = () => {
-    return (
-        <div>
-            <h2>Calendario</h2>
-        </div>
-    )
-}
+  return (
+    <table class="table table-hover">
+      <thead>
+        <tr>
+          <th scope="col" className='cabeceraTabla'> Horario</th>
+          <th scope="col" className='cabeceraTabla'> Lunes a Viernes </th>
+          <th scope="col" className='cabeceraTabla'>Sábado</th>
+          <th scope="col" className='cabeceraTabla'>Domingo</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row" className='horaTabla'> 05:00</th>
+          <td rowspan="4" className='reservado' > Latin noticias: primera edición </td>
+          <td className='cincoRecargo' > Latin espectáculos </td>
+          <td className='reservado' > Automundo </td>
+        </tr>
+        <tr>
+          <th scope="row" className='horaTabla' > 06:00</th>
+          <td className='cincoRecargo'> Latin noticias edición sábados </td>
+          <td className='reservado'> Estás en Todas (repetición)</td>
+        </tr>
+        <tr>
+          <th scope="row" className='horaTabla'> 07:00</th>
+          <td rowspan="2" className='quinceRecargo'> Latin noticias edición sábados </td>
+          <td className='quinceRecargo'> Latin noticias edición sábados </td>
+        </tr>
+        <tr>
+          <th scope="row" className='horaTabla'> 08:00</th>
+          <td className='sinRecargo'> Otro programa </td>
+        </tr>
+        <tr>
+          <th scope="row" className='horaTabla' > 09:00</th>
+          <td className='reservado'>El privilegio de amar</td>
+          <td className='cincoRecargo'> Latin noticias edición sábados </td>
+          <td className='reservado'> Estás en Todas (repetición)</td>
+        </tr>
+        <tr>
+          <th scope="row" className='horaTabla'> 10:00</th>
+          <td rowspan="2" className='cincoRecargo'>Otro programa</td>
+          <td className='quinceRecargo'> Latin noticias edición sábados </td>
+          <td className='quinceRecargo'> Latin noticias edición sábados </td>
+        </tr>
+        <tr>
+          <th scope="row" className='horaTabla'> 11:00</th>
+          <td className='quinceRecargo'> Latin noticias edición sábados </td>
+          <td rowspan="2" className='sinRecargo'> Otro programa </td>
+        </tr>
+        <tr>
+          <th scope="row" className='horaTabla'> 12:00</th>
+          <td className='reservado' > Latin noticias: primera edición </td>
+          <td className='cincoRecargo' > Latin espectáculos </td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
--- src/Vistas/Programacion.jsx
+++ src/Vistas/Programacion.jsx
@@ -3,12 +3,42 @@ import React from 'react';
 import { Cabecera } from '../componentes/Cabecera';
 import { Calendario } from '../componentes/Calendario';
 
+import '../assets/estilos/vistas/programacion.scss';
+
 const Programacion = () => (
   <>
     <Cabecera />
-    <Calendario />
+    <div className="contenedorProgramacion">
+      <h1>Bienvenido!</h1>
+      <p className='infoProgramacion'>
+        Aquí puedes informarte sobre la programación de Latin TV.{' '}
+        <strong>
+          Para reservar un espacio publicitario dirígete al menú lateral
+        </strong>
+      </p>
+      <div className="contenedorPrograma">
+        <div className="opcionesRecargo">
+          <div className="opcion">
+            <div className=" caja reservado"></div>
+            <p>Reservado</p>
+          </div>
+          <div className="opcion">
+            <div className="caja sinRecargo"></div>
+            <p>Sin recargo</p>
+          </div>
+          <div className="opcion">
+            <div className="caja cincoRecargo"></div>
+            <p>5% de recargo</p>
+          </div>
+          <div className="opcion">
+            <div className="caja quinceRecargo"></div>
+            <p>15% de recargo</p>
+          </div>
+        </div>
+        <Calendario />
+      </div>
+    </div>
   </>
 );
 
 export default Programacion;
-
