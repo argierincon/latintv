c9beb25e0126b9cc4d304afe6a12703298723986
verenisseeyzaguirre <verenisse.eyzaguirre@gmail.com>
2020-09-01 11:02:57 -0500

    estructura y nombre de componentes

---
+++ src/Componentes/Calendario.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Calendario = () => {
+    return (
+        <div>
+            <h2>Calendario</h2>
+        </div>
+    )
+}
---
+++ src/Rutas/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Programacion from '../vistas/Programacion';
+import Reserva from '../Vistas/ReservaEspacio';
+
+
+const App = () => (
+  <BrowserRouter>
+    <Route exact path='/' component={Programacion} />
+    <Route exact path='/reserva' component={Reserva} />
+  </BrowserRouter>
+);
+
+export default App;
