e78fa815cdbf1e87b747a92c4c7d2e36c55349a7
argierdfj <argierincon@gmail.com>
2020-09-02 00:08:08 -0500

    añadiendo ruta selecciona programa

--- src/Rutas/App.js
+++ src/Rutas/App.js
@@ -3,13 +3,14 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Programacion from '../vistas/Programacion';
 import Reserva from '../vistas/ReservaEspacio';
 import FlujoFechaHora from '../vistas/FlujoFechaHora';
-
+import FlujoProgramacion from '../Vistas/FlujoProgramacion';
 
 const App = () => (
   <BrowserRouter>
-    <Route exact path='/' component={Programacion} />
-    <Route exact path='/reserva' component={Reserva} />
-    <Route exact path='/procesofechahora' component={FlujoFechaHora} />
+    <Route exact path="/" component={Programacion} />
+    <Route exact path="/reserva" component={Reserva} />
+    <Route exact path="/reservaprogramacion" component={FlujoProgramacion} />
+    <Route exact path="/procesofechahora" component={FlujoFechaHora} />
   </BrowserRouter>
 );
 
