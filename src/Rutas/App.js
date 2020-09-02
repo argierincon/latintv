00525d99b2c10d31fdd0a23f402b90092d1a7565
BBQ <erikaquispejanampa@gmail.com>
2020-09-01 22:18:14 -0500

    Ruteo de vistas a la barra lateral

--- src/Rutas/App.js
+++ src/Rutas/App.js
@@ -3,11 +3,15 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Programacion from '../vistas/Programacion';
 import Reserva from '../vistas/ReservaEspacio';
 import FlujoFechaHora from '../vistas/FlujoFechaHora';
-
+import Navbar from '../Componentes/BarraLateral/BarraLateral';
+import Inicio from '../Vistas/Inicio'
 
 const App = () => (
+ 
   <BrowserRouter>
-    <Route exact path='/' component={Programacion} />
+  <Navbar />
+    <Route exact path='/' component={Inicio} />
+    <Route exact path='/programacion' component={Programacion} />
     <Route exact path='/reserva' component={Reserva} />
     <Route exact path='/procesofechahora' component={FlujoFechaHora} />
   </BrowserRouter>
