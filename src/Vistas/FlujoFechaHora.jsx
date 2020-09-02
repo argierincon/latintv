e76431b6f73b85ac376feb9f9bc853aa2f5c3cad
verenisseeyzaguirre <verenisse.eyzaguirre@gmail.com>
2020-09-01 20:03:12 -0500

    componente flujoProceso complete

---
+++ src/Vistas/FlujoFechaHora.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { FlujoProceso } from '../componentes/FlujoProceso';
+
+const FlujoFechaHora = () => {
+  return (
+    <div>
+      <FlujoProceso />
+    </div>
+  );
+};
+
+export default FlujoFechaHora;
