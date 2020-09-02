201865bc25991fa62145204af503dac2cb37db75
verenisseeyzaguirre <verenisse.eyzaguirre@gmail.com>
2020-09-01 21:01:17 -0500

    infoHead componente

--- src/Vistas/FlujoFechaHora.jsx
+++ src/Vistas/FlujoFechaHora.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { FlujoProceso } from '../componentes/FlujoProceso';
+import { InfoHead } from '../componentes/InfoHead';
 
 const FlujoFechaHora = () => {
   return (
     <div>
       <FlujoProceso />
+      <InfoHead titulo="Elige fecha y hora" />
     </div>
   );
 };
