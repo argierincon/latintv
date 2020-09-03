06cbafd9df6833df4faf8993d525c93d9f3b01c0
BBQ <erikaquispejanampa@gmail.com>
2020-09-03 10:07:59 -0500

    Pie de página

---
+++ src/Componentes/PiePagina/Footer.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import './Footer.scss';
+
+const Footer = () => (
+  <div className="footer">
+    <table class="footer-font">
+      <tbody>
+        <tr>
+          <td>CATEGORÍAS</td>
+          <td></td>
+          <td> INFORMACIÓN AL USUARIO</td>
+          <td></td>
+        </tr>
+
+        <tr>
+          <td>Noticias</td>
+          <td></td>
+          <td>Políticas de seguridad y salud en el trabajo</td>
+          <td>Logo Fb</td>
+        </tr>
+
+        <tr>
+        <td>Deportes</td>
+        <td></td>
+        <td>Políticas de privacidad</td>
+        <td>Logo Instagram</td>
+      </tr>
+
+      <tr>
+      <td>Entretenimiento</td>
+      <td></td>
+      <td>Términos y condiciones</td>
+      <td>Logo twiter</td>
+    </tr>
+
+    <tr>
+    <td>Publicidad</td>
+    <td></td>
+    <td>Código de ética</td>
+  </tr>
+
+        <tr>
+        <td></td>
+        <td></td>
+        <td>Contáctanos</td>
+      </tr>
+
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+);
+
+export default Footer;
